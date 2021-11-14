bc332f0906a0a82afdb656fffb073061316a18aa
harrywatkinson <harry@revmo.app>
2021-11-14 11:23:20 +0000

    black

--- board.py
+++ board.py
@@ -1,3 +1,17 @@
+from enum import Enum
+from typing import Union
+
+
+class Markers(Enum):
+    PLAYER_1 = "X"
+    PLAYER_2 = "O"
+    CLEAR = " "
+    BOTTOM_0 = "0"
+    BOTTOM_1 = "1"
+    BOTTOM_2 = "2"
+    BOTTOM_3 = "3"
+    BOTTOM_4 = "4"
+    BOTTOM_5 = "5"
 
 
 class Board:
@@ -6,14 +20,82 @@ class Board:
     PLAYER_2 = "O"
 
     board = [
-        [" ", " ", " ", " ", " "],
-        [" ", " ", " ", " ", " "],
-        [" ", " ", " ", " ", " "],
-        [" ", " ", " ", " ", " "],
-        ["_", "_", "_", "_", "_"],
-        ["0", "1", "2", "3", "4"],
+        [
+            Markers.CLEAR,
+            Markers.CLEAR,
+            Markers.CLEAR,
+            Markers.CLEAR,
+            Markers.CLEAR,
+            Markers.CLEAR,
+        ],
+        [
+            Markers.CLEAR,
+            Markers.CLEAR,
+            Markers.CLEAR,
+            Markers.CLEAR,
+            Markers.CLEAR,
+            Markers.CLEAR,
+        ],
+        [
+            Markers.CLEAR,
+            Markers.CLEAR,
+            Markers.CLEAR,
+            Markers.CLEAR,
+            Markers.CLEAR,
+            Markers.CLEAR,
+        ],
+        [
+            Markers.CLEAR,
+            Markers.CLEAR,
+            Markers.CLEAR,
+            Markers.CLEAR,
+            Markers.CLEAR,
+            Markers.CLEAR,
+        ],
+        [
+            Markers.CLEAR,
+            Markers.CLEAR,
+            Markers.CLEAR,
+            Markers.CLEAR,
+            Markers.CLEAR,
+            Markers.CLEAR,
+        ],
+        [
+            Markers.CLEAR,
+            Markers.CLEAR,
+            Markers.CLEAR,
+            Markers.CLEAR,
+            Markers.CLEAR,
+            Markers.CLEAR,
+        ],
+        [
+            Markers.BOTTOM_0,
+            Markers.BOTTOM_1,
+            Markers.BOTTOM_2,
+            Markers.BOTTOM_3,
+            Markers.BOTTOM_4,
+            Markers.BOTTOM_5,
+        ],
     ]
 
     def display_board(self):
         for row in self.board:
-            print("|".join(row))
+            print("|".join([square.value for square in row]))
+
+    def to_bottom(self, player_choice: int):
+
+        for column, row in enumerate(self.board, -1):
+
+            square = row[player_choice]
+            free_column = column
+            if square != Markers.CLEAR:
+                return free_column
+
+    def place_counter(
+        self,
+        player_counter: Union[Markers.PLAYER_1, Markers.PLAYER_2],
+        player_choice: int,
+        free_column: int,
+    ):
+
+        self.board[free_column][player_choice] = player_counter
